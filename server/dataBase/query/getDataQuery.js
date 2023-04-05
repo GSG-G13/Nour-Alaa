4dbee793a3a072443b4a914c6278c87cef480df8
alaaahmade <alaaahmade66@gmail.com>
2023-04-05 12:39:30 +0200

    create getData query to get all data from database Relates #12

--- server/dataBase/query/getDataQuery.js
+++ server/dataBase/query/getDataQuery.js
@@ -1,5 +1,4 @@
 const connection = require('../connection');
 
 const getData = () => connection.query('select * from products join catogery on products.catogery_id=catogery.id');
- getData().then(console.log);
 module.exports = getData;
