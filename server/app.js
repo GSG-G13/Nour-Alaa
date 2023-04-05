e507792c2addcd50da48edc8e2daa01d1c979c02
alaaahmade <alaaahmade66@gmail.com>
2023-04-05 11:58:20 +0200

    create connection file Relates 12

--- server/app.js
+++ server/app.js
@@ -5,10 +5,10 @@ const router = require('./routers/router');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+app.set('PORT', process.env.PORT || 8080);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use(router);
 
-module.exports = { app, PORT };
+module.exports = app;
